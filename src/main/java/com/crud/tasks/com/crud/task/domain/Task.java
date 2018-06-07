b773c8ad971b89f292b6af87a9d6b6eb3a08d3d6
MattStefanski <mateusz.stefanski.eu@gmail.com>
2018-06-07 20:15:18 +0200

    Initial commit

---
+++ src/main/java/com/crud/tasks/com/crud/task/domain/Task.java
@@ -0,0 +1,13 @@
+package com.crud.tasks.com.crud.task.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+
+@Getter
+@AllArgsConstructor
+public class Task {
+    private Long id;
+    private String title;
+    private String content;
+}
